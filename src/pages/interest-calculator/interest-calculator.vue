4a95d22900bdc1c608c2dcdad84d234cb0a4917e
yanmiao99 <503084944@qq.com>
2023-01-23 01:08:51 +0800

    feat : 利息计算器 - 公式完成

--- src/pages/interest-calculator/interest-calculator.vue
+++ src/pages/interest-calculator/interest-calculator.vue
@@ -2,33 +2,28 @@
   <view class="wrapper">
     <u--form labelPosition="top" :model="model" :rules="rules" ref="uForm" labelWidth="200rpx">
 
-      <u-form-item label="存入金额 (元) " prop="info.amount" border>
-        <u--input clearable v-model="model.info.amount" placeholder="请输入金额" prefixIcon="rmb-circle"/>
+      <u-form-item label="存入金额 (元) " prop="amount" border>
+        <u--input clearable v-model="model.amount" placeholder="请输入金额" prefixIcon="rmb-circle"/>
       </u-form-item>
 
-      <u-form-item label="每天收益 (元)" prop="info.profit" border>
-        <u--input clearable v-model="model.info.profit" placeholder="请输入每天得到的收益" prefixIcon="rmb-circle"/>
+      <u-form-item label="每天收益 (元)" prop="profit" border>
+        <u--input clearable v-model="model.profit" placeholder="请输入每天得到的收益" prefixIcon="rmb-circle"/>
       </u-form-item>
 
-      <u-form-item label="日利率 (%) " prop="info.daily" border>
-        <u--input v-model="model.info.daily" :disabled="true" placeholder="自动计算得出"
+      <u-form-item label="日利率 (%) " prop="daily" border>
+        <u--input v-model="model.daily" :disabled="true" placeholder="自动计算得出"
                   prefixIcon="rmb-circle-fill"/>
       </u-form-item>
 
-      <u-form-item label="月利率 (%) " prop="info.month" border>
-        <u--input v-model="model.info.month" :disabled="true" placeholder="自动计算得出"
+      <u-form-item label="月利率 (%) " prop="month" border>
+        <u--input v-model="model.month" :disabled="true" placeholder="自动计算得出"
                   prefixIcon="rmb-circle-fill"/>
       </u-form-item>
 
-      <u-form-item label="年利率 (%) " prop="info.year" border>
-        <u--input v-model="model.info.year" :disabled="true" placeholder="自动计算得出"
+      <u-form-item label="年利率 (%) " prop="year" border>
+        <u--input v-model="model.year" :disabled="true" placeholder="自动计算得出"
                   prefixIcon="rmb-circle-fill"/>
       </u-form-item>
-
-      <!--<u-form-item label="实际到账 (元) " prop="info.actualAccount" border>-->
-      <!--  <u&#45;&#45;input v-model="model.info.actualAccount" :disabled="true" placeholder="自动计算得出"-->
-      <!--            prefixIcon="rmb-circle-fill"/>-->
-      <!--</u-form-item>-->
 
       <u-form-item>
         <u-button class="btn" type="primary" text="计 算" @tap="submit"/>
@@ -43,7 +38,7 @@
       <view>
         日利率是1/10000=0.01% <br/>
         月利率就是30*1/10000=0.3% <br/>
-        年利率就是1/10000*360=3.65%。<br/>
+        年利率就是1/10000*360=3.60%。<br/>
       </view>
     </view>
   </view>
@@ -58,22 +53,20 @@ export default {
   data() {
     return {
       model: {
-        info: {
-          amount: '', // 金额
-          profit: '', // 每日收益
-          daily: '', // 日利率
-          month: '', // 月利率
-          year: '', // 年利率
-        }
+        amount: '', // 金额
+        profit: '', // 每日收益
+        daily: '', // 日利率
+        month: '', // 月利率
+        year: '', // 年利率
       },
       rules: {
-        'info.amount': {
+        'amount': {
           type: 'number',
           required: true,
           message: '请填写数字',
           trigger: ['blur', 'change']
         },
-        'info.profit': {
+        'profit': {
           type: 'number',
           required: true,
           message: '请填写数字',
@@ -86,9 +79,14 @@ export default {
     // 提交事件
     submit() {
       this.$refs.uForm.validate().then(() => {
+        const {amount, profit} = this.model
 
         // 公式计算
+        const daily = ((profit / amount) * 100).toFixed(4)
+        const month = ((30 * profit / amount) * 100).toFixed(4)
+        const year = ((profit / amount * 360) * 100).toFixed(4)
 
+        this.model = {amount, profit, daily, month, year}
 
       }).catch(errors => {
         uni.$u.toast('校验失败')
